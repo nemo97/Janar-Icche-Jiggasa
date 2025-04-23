9042b5de4c4576b06396027cc01d66a5f47dd028
Subhas Sing <subhas_sing@yahoo.com>
2025-04-23 14:47:25 -0700

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/nemo97/Janar-Icche-Jingasa/tree/main/docs',
+            'https://github.com/nemo97/Janar-Icche-Jingasa/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/nemo97/Janar-Icche-Jingasa/tree/main/docs',
+            'https://github.com/nemo97/Janar-Icche-Jingasa/tree/main',
         },
         theme: {
           customCss: './src/css/custom.css',
