649d1604ff1005f73a0a98b844a4b4b89ae5ca20
Subhas Sing <subhas_sing@yahoo.com>
2025-05-01 13:33:34 -0700

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://www.janar-icche-jiggasha.in',
+  url: 'https://nemo97.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
